022179c824546dde9126150ada9c4a7c448038ab
Nghi Le <vinhnghi223@gmail.com>
2014-02-12 22:51:25 +0200

    update

--- Gallery_images_jsevents/script.js
+++ Gallery_images_jsevents/script.js
@@ -2,14 +2,19 @@ document.querySelector('.grid').addEventListener('mouseover', function(e) {
   var etarget=e.target;
   if (etarget.tagName === 'IMG') {
 
+    //show magnifying glass
     var elm = document.createElement('div');
     elm.className = 'preview';
     etarget.parentNode.appendChild(elm);
 
+    //img elm
     var img = document.createElement('img');
     var imgName = etarget.src;
     img.src = imgName.substr(0,imgName.length-4)+'o.jpg';
     elm.appendChild(img);
+
+    elm.style.left=e.offsetX+15+'px';
+    elm.style.top=e.offsetY+15+'px';
 
     etarget.addEventListener('mouseout', function handler(d) {
       var myNode = d.target.parentNode.querySelector('div.preview');
@@ -17,5 +22,9 @@ document.querySelector('.grid').addEventListener('mouseover', function(e) {
       e.target.removeEventListener('mouseout', handler, false);
     }, false);
 
+    etarget.addEventListener('mousemove',function(f){
+      elm.style.left=f.offsetX+15+'px';
+      elm.style.top=f.offsetY+15+'px';
+    },false);
   } // check if mouse is over images
 }, false); // mouseover event
