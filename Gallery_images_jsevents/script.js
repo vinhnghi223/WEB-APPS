429e5c8d9680807abf5e995dcd0e9c5ced471072
Nghi Le <vinhnghi223@gmail.com>
2014-02-12 23:44:52 +0200

    add spinner and overlay

--- Gallery_images_jsevents/script.js
+++ Gallery_images_jsevents/script.js
@@ -26,5 +26,27 @@ document.querySelector('.grid').addEventListener('mouseover', function(e) {
       elm.style.left=f.offsetX+15+'px';
       elm.style.top=f.offsetY+15+'px';
     },false);
+
+    etarget.addEventListener('click',function(g){
+      var lowRes=g.target.src;
+      console.log(lowRes);
+      var overLay=document.querySelector('.overlay');
+      var highRes=document.createElement('img');
+      var spinner=document.createElement('img');
+
+      highRes.src=lowRes.substr(0,imgName.length-4)+'o.jpg';
+      highRes.className='bgImg';
+      spinner.src='images/spinner.gif';
+      spinner.className='spinner';
+      
+      overLay.style.display='block';
+      overLay.appendChild(spinner);
+      overLay.appendChild(highRes);
+
+
+      highRes.addEventListener('load',function(){
+        overLay.removeChild(spinner);
+      })
+    },false);
   } // check if mouse is over images
 }, false); // mouseover event
